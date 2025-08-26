8bf57544a58722e99c25142b6c9710ed3a418445
alfian-bangunindo <bahruL@bangunindo.com>
2025-08-26 21:40:47 +0700

    refactor: use env variable as kafka brokers and kafka topic by default

--- src/data_streamer/producer.py
+++ src/data_streamer/producer.py
@@ -2,7 +2,7 @@ import json
 
 from kafka import KafkaProducer
 
-from src.config import KAFKA_BOOTSTRAP_SERVERS, KAFKA_TOPIC
+from src.config import KAFKA_BOOTSTRAP_SERVERS, KAFKA_TOPIC_SOURCE
 
 
 class KafkaProducerWrapper:
@@ -10,34 +10,31 @@ class KafkaProducerWrapper:
     A wrapper class for Kafka producer to send messages to a Kafka topic.
     """
 
-    def __init__(self, topic_name: str | None = None):
+    def __init__(
+        self,
+        kafka_bootstrap_servers: str | None = KAFKA_BOOTSTRAP_SERVERS,
+        kafka_topic_source: str | None = KAFKA_TOPIC_SOURCE,
+    ):
         """
         Initialize the Kafka producer with the provided or environment variable configurations.
 
         Args:
-            topic_name (str, optional): The Kafka topic name to consume from. If None, uses KAFKA_TOPIC from environment variables. Defaults to None.
+            kafka_topic_source (str, optional): The Kafka topic name to consume from. If None, uses KAFKA_TOPIC from environment variables. Defaults to None.
         """
         print("STARTING KAFKA PRODUCER...")
 
-        if KAFKA_BOOTSTRAP_SERVERS:
-            self.bootstrap_servers = KAFKA_BOOTSTRAP_SERVERS
-        else:
+        if not kafka_bootstrap_servers:
             raise ValueError(
                 "KAFKA_BOOTSTRAP_SERVERS is not set in environment variables."
             )
+        self.kafka_bootstrap_servers = kafka_bootstrap_servers
 
-        if topic_name is not None:
-            self.topic_name = topic_name
-        else:
-            print(
-                "'topic_name' is not provided, using KAFKA_TOPIC from environment variables."
-            )
-            if KAFKA_TOPIC is None:
-                raise ValueError("KAFKA_TOPIC is not set in environment variables.")
-            self.topic_name = KAFKA_TOPIC
+        if not kafka_topic_source:
+            raise ValueError("KAFKA_TOPIC_SOURCE is not set in environment variables.")
+        self.kafka_topic_source = kafka_topic_source
 
         self.producer = KafkaProducer(
-            bootstrap_servers=self.bootstrap_servers,
+            bootstrap_servers=self.kafka_bootstrap_servers,
             value_serializer=lambda v: json.dumps(v).encode("utf-8"),
         )
 
@@ -48,9 +45,9 @@ class KafkaProducerWrapper:
         """
         Send data to the Kafka topic.
         """
-        self.producer.send(self.topic_name, value=data)
+        self.producer.send(self.kafka_topic_source, value=data)
 
-        print("Data sent to Kafka topic:", self.topic_name)
+        print("Data sent to Kafka topic:", self.kafka_topic_source)
 
     def close(self):
         """
