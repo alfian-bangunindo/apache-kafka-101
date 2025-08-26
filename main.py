5f338629c306070bb180a277bfa3259fc4808e48
alfian-bangunindo <bahruL@bangunindo.com>
2025-08-26 21:44:28 +0700

    feat: adjust with the spark streaming

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import random
 import time
 from argparse import ArgumentParser
 
-from src.config import NUM_DEVICES
+from pyspark.sql import SparkSession
+
+from src.config import JSON_SCHEMA, NUM_DEVICES
 from src.data_generator.weather_station import DummyDataWeatherStation
-from src.data_streamer.consumer import KafkaConcumerWrapper
-from src.data_streamer.database_handler import DatabaseHandler
+from src.data_streamer.consumer import SparkConsumer
 from src.data_streamer.producer import KafkaProducerWrapper
-from src.utils import transform_weather_data
+from src.data_transformer.aggregation_transformer import AggregationTransformer
+from src.data_transformer.flatten_transformer import FlattenJSONTransformer
 
 if __name__ == "__main__":
     parser = ArgumentParser(
-        description="Simulate streaming data from weather station devices to Kafka topic."
+        description="Simulate streaming processing of weather station data to Kafka topic and database."
     )
     parser.add_argument("--role")
     args = parser.parse_args()
 
     if args.role == "produce_raw_data":
         # Produce raw data to Kafka topic
-        raw_data_producer = KafkaProducerWrapper()
+        producer = KafkaProducerWrapper()
         data_generator = DummyDataWeatherStation(NUM_DEVICES)
         try:
             while True:
                 data = data_generator.next_data_stream()
-                raw_data_producer.send(data)
+                producer.send(data)
                 time.sleep(random.uniform(1, 2))
                 print("\n")
 
@@ -31,25 +33,37 @@ if __name__ == "__main__":
             print("Data streaming is done!")
 
         finally:
-            raw_data_producer.close()
+            producer.close()
 
-    elif args.role == "consume_raw_data":
-        # Consume raw data from Kafka topic, transform it, and store it to another Kafka topic and PostgreSQL database
-        raw_data_consumer = KafkaConcumerWrapper(transform_weather_data)
-        transform_data_producer = KafkaProducerWrapper(
-            topic_name="weather_station_transformed"
+    elif args.role == "transform_data":
+        # Initialize spark session
+        spark = (
+            SparkSession.builder.appName("Weather Station Data Streaming")
+            .config("spark.streaming.stopGracefullyOnShutdown", True)
+            .config("spark.sql.shuffle.partitions", 4)
+            .config(
+                "spark.jars.packages",
+                "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0,"
+                "org.postgresql:postgresql:42.2.20",
+            )
+            .master("local[*]")
+            .getOrCreate()
         )
-        db_handler = DatabaseHandler()
 
-        try:
-            for data in raw_data_consumer.consume():
-                transform_data_producer.send(data)
-                db_handler.insert_data(data)
+        # Consume data from kafka topic using spark
+        consumer = SparkConsumer(spark)
+        raw_df = consumer.read_from_kafka(JSON_SCHEMA)
 
-                print("\n")
+        # Flatten raw dataframe
+        flatten_transformer = FlattenJSONTransformer(spark)
+        flatten_query = flatten_transformer.process(raw_df, "kafka")
 
-        except KeyboardInterrupt:
-            print("Data consuming is done!")
+        # Aggregate flattened dataframe
+        flattened_df = flatten_transformer.transform(raw_df)
+        aggregate_transformer = AggregationTransformer(spark)
+        agg_query = aggregate_transformer.process(flattened_df, "db")
+
+        spark.streams.awaitAnyTermination()
 
     else:
-        print("Please provide a valid role: produce_raw_data or consume_raw_data")
+        print("Please provide a valid role: produce_raw_data or transform_data")
